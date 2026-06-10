7578e8590e78658793f51c883ebfed1d9274804e
kingcedru <byishimocedrick@gmail.com>
2026-06-10 11:57:41 +0200

    add the base class

--- app/db/database.py
+++ app/db/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, DeclarativeBase, sessionmaker
 from dotenv import load_dotenv
 import os
 
@@ -12,4 +12,5 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
